6844cfcbf5cde0dc6df29aadce2dbfd26a8128a0
Bob Fratantonio <bobfrat@gmail.com>
2017-11-09 19:07:46 -0500

    Hardcode app to be deployed to subfolder /data

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/data', express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/data', routes);
+app.enable('trust proxy');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
